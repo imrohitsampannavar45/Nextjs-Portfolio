5a59f8761035224e878f78d03ce169293e5233ee
imrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-09-20 11:42:57 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+// next.config.mjs
+
+export default {
+  images: {
+    domains: ['myprojectvideos123.s3.ap-south-1.amazonaws.com'],
+  },
+};
---
+++ src/lib/utils.ts
@@ -0,0 +1,40 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: string) {
+  let currentDate = new Date().getTime();
+  
+  // Check if date is valid
+  if (!date.includes("T")) {
+    date = `${date}T00:00:00`; // Use backticks for template literals
+  }
+
+  let targetDate = new Date(date).getTime();
+  let timeDifference = Math.abs(currentDate - targetDate);
+  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  let fullDate = new Date(date).toLocaleString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  if (daysAgo < 1) {
+    return "Today";
+  } else if (daysAgo < 7) {
+    return `${fullDate} (${daysAgo}d ago)`; // Use backticks here
+  } else if (daysAgo < 30) {
+    const weeksAgo = Math.floor(daysAgo / 7);
+    return `${fullDate} (${weeksAgo}w ago)`; // Use backticks here
+  } else if (daysAgo < 365) {
+    const monthsAgo = Math.floor(daysAgo / 30);
+    return `${fullDate} (${monthsAgo}mo ago)`; // Use backticks here
+  } else {
+    const yearsAgo = Math.floor(daysAgo / 365);
+    return `${fullDate} (${yearsAgo}y ago)`; // Use backticks here
+  }
+}
